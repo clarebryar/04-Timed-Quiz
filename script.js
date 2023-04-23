a9b59979d215fff9380479577c758ce7ebc9a1b6
clarebryar <128009509+clarebryar@users.noreply.github.com>
2023-04-23 14:57:26 -0500

    Starting quiz project

---
+++ script.js
@@ -0,0 +1,23 @@
+//Questions array 
+var questions = ["question1",
+];
+
+var answers = [
+ "answer1"];
+
+
+function startQuiz(){
+    for (var i = 0; i < questions.length; i++);
+
+for (var i = 0; i < answers.length; i++);
+document.querySelector("questions").innerHTML = startQuiz();
+}
+
+
+
+//Click button to start quiz
+//save.addEventListener("click", function(event) {
+ //   event.preventDefault(); {
+ //   }
+ //   console.log(event);
+//});
