8b4549ae9e5e898630c909b93c347f110bbb3f8a
clarebryar <128009509+clarebryar@users.noreply.github.com>
2023-04-26 18:25:42 -0500

    added styling and buttons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Questions array
 
-var startButton = document.querySelector("#start")
-
+var startButton = document.querySelector("#start");
+var nextButton = document.querySelector("#nextButton");
 var currentQuestion;
 
 var quizQuestions = [
@@ -11,59 +11,49 @@ var quizQuestions = [
     option2: "4",
     option3: "answer3",
     option4: "answer4",
-    answer: 1
+    answer: 1,
   },
   {
     question: "question numero dos",
-    options1: "answer1",
-    option2:  "answer2",
+    option1: "answer1",
+    option2: "answer2",
     option3: "answer3",
     option4: "answer4",
-    answer: 2
+    answer: 2,
   },
 ];
-var questionEl = document.querySelector("#questions")
-var answerListEl = document.querySelector("#answersList")
+var questionEl = document.querySelector("#questions");
+var answerListEl = document.querySelector("#answersList");
 
 var answer1 = document.querySelector("#answer1");
 var answer2 = document.querySelector("#answer2");
 var answer3 = document.querySelector("#answer3");
 var answer4 = document.querySelector("#answer4");
 
+var questionCount = 0;
 
-
-var questionCount = 0 
-
+answerListEl.style.display = "none";
+nextButton.style.display = "none"; 
 
 //Click button to start quiz
 startButton.addEventListener("click", function (event) {
   event.preventDefault();
   console.log(event);
   showQuiz();
+  startButton.style.display = "none";
 });
 
-
 function showQuiz() {
-var currentQuestion =  quizQuestions[questionCount];
-questionEl.innerHTML = quizQuestions[questionCount].question;
-answer1.textContent = quizQuestions[questionCount].option1;
-answer2.textContent = quizQuestions[questionCount].option2;
-answer3.textContent= quizQuestions[questionCount].option3;
-answer4.textContent = quizQuestions[questionCount].option4;
-questionCount++
-   
-//for (var i = 0; i < options.quizQuestions[0].options.length; i++) {
-  //answersClass
-//}
-  
-  ///answersClass.forEach(function(element, index) {
- //   element.textContent = q.options[index]
-   //});
-};
-
-
-
-//function displayQuizContent() {
-//   
-  
-//}
+    //var currentQuestion =  quizQuestions[questionCount];
+  nextButton.style.display = "inline";
+  nextButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    answerListEl.style.display = "inline";
+    questionEl.innerHTML = quizQuestions[questionCount].question;
+    answer1.textContent = quizQuestions[questionCount].option1;
+    answer2.textContent = quizQuestions[questionCount].option2;
+    answer3.textContent = quizQuestions[questionCount].option3;
+    answer4.textContent = quizQuestions[questionCount].option4;
+    questionCount++;
+  });
+  }
