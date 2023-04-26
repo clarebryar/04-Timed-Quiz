49e719deff5542bdc4c51a7a172f6f7e826e00c1
clarebryar <128009509+clarebryar@users.noreply.github.com>
2023-04-26 11:44:19 -0500

    got the quiz content to display on the page

--- script.js
+++ script.js
@@ -7,19 +7,30 @@ var currentQuestion;
 var quizQuestions = [
   {
     question: "question1 will think of a better question later",
-    options: ["answer1", "answer2", "answer3", "answer4"],
+    option1: "right answer",
+    option2: "4",
+    option3: "answer3",
+    option4: "answer4",
     answer: 1
   },
   {
     question: "question numero dos",
-    options: ["answer1",  "answer2", "answer3", "answer4","answer3"],
+    options1: "answer1",
+    option2:  "answer2",
+    option3: "answer3",
+    option4: "answer4",
     answer: 2
   },
 ];
 var questionEl = document.querySelector("#questions")
 var answerListEl = document.querySelector("#answersList")
 
-var answersClass = document.querySelectorAll(".answers")
+var answer1 = document.querySelector("#answer1");
+var answer2 = document.querySelector("#answer2");
+var answer3 = document.querySelector("#answer3");
+var answer4 = document.querySelector("#answer4");
+
+
 
 var questionCount = 0 
 
@@ -27,24 +38,32 @@ var questionCount = 0
 //Click button to start quiz
 startButton.addEventListener("click", function (event) {
   event.preventDefault();
-console.log(event);
-var currentQuestion =  quizQuestions[questionCount];
-   console.log(currentQuestion);
-
-   questionEl.innerHTML = currentQuestion.question;
-   answersClass.innerHTML = currentQuestion.options;
-   questionCount++
+  console.log(event);
+  showQuiz();
 });
 
-//function displayQuizContent() {
-//   var currentQuestion =  quizQuestions[questionCount];
-  // console.log(currentQuestion);
 
-   //questionEl.innerHTML = currentQuestion.question;
-   //answer1El.innerHTML = currentQuestion.option1;
-   //answer2El.innerHTML = currentQuestion.option2;
-   //answer3El.innerHTML = currentQuestion.option3;
-   //answer4El.innerHTML = currentQuestion.option4;
-   //questionCount++
+function showQuiz() {
+var currentQuestion =  quizQuestions[questionCount];
+questionEl.innerHTML = quizQuestions[questionCount].question;
+answer1.textContent = quizQuestions[questionCount].option1;
+answer2.textContent = quizQuestions[questionCount].option2;
+answer3.textContent= quizQuestions[questionCount].option3;
+answer4.textContent = quizQuestions[questionCount].option4;
+questionCount++
+   
+//for (var i = 0; i < options.quizQuestions[0].options.length; i++) {
+  //answersClass
+//}
+  
+  ///answersClass.forEach(function(element, index) {
+ //   element.textContent = q.options[index]
+   //});
+};
+
+
+
+//function displayQuizContent() {
+//   
   
 //}
