ba233389c543e96fe34e23371b20034edce45b8d
clarebryar <128009509+clarebryar@users.noreply.github.com>
2023-04-26 08:25:54 -0500

    re-arranged answers into array

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-//Questions array 
-var questions = ["question1",
+//Questions array
+
+var startButton = document.querySelector("#start")
+
+var currentQuestion;
+
+var quizQuestions = [
+  {
+    question: "question1 will think of a better question later",
+    options: ["answer1", "answer2", "answer3", "answer4"],
+    answer: 1
+  },
+  {
+    question: "question numero dos",
+    options: ["answer1",  "answer2", "answer3", "answer4","answer3"],
+    answer: 2
+  },
 ];
+var questionEl = document.querySelector("#questions")
+var answerListEl = document.querySelector("#answersList")
 
-var answers = [
- "answer1"];
+var answersClass = document.querySelectorAll(".answers")
 
-
-function startQuiz(){
-    for (var i = 0; i < questions.length; i++);
-
-for (var i = 0; i < answers.length; i++);
-document.querySelector("questions").innerHTML = startQuiz();
-}
-
+var questionCount = 0 
 
 
 //Click button to start quiz
-//save.addEventListener("click", function(event) {
- //   event.preventDefault(); {
- //   }
- //   console.log(event);
-//});
+startButton.addEventListener("click", function (event) {
+  event.preventDefault();
+console.log(event);
+var currentQuestion =  quizQuestions[questionCount];
+   console.log(currentQuestion);
+
+   questionEl.innerHTML = currentQuestion.question;
+   answersClass.innerHTML = currentQuestion.options;
+   questionCount++
+});
+
+//function displayQuizContent() {
+//   var currentQuestion =  quizQuestions[questionCount];
+  // console.log(currentQuestion);
+
+   //questionEl.innerHTML = currentQuestion.question;
+   //answer1El.innerHTML = currentQuestion.option1;
+   //answer2El.innerHTML = currentQuestion.option2;
+   //answer3El.innerHTML = currentQuestion.option3;
+   //answer4El.innerHTML = currentQuestion.option4;
+   //questionCount++
+  
+//}
