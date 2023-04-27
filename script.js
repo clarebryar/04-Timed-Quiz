87304918f8a52f5579ad7e8e5c608af57fabab73
clarebryar <128009509+clarebryar@users.noreply.github.com>
2023-04-27 08:31:00 -0500

    working on timer

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 var startButton = document.querySelector("#start");
 var nextButton = document.querySelector("#nextButton");
+
 var currentQuestion;
 
 var quizQuestions = [
@@ -11,7 +12,7 @@ var quizQuestions = [
     option2: "4",
     option3: "answer3",
     option4: "answer4",
-    answer: 1,
+    answer: "4",
   },
   {
     question: "question numero dos",
@@ -19,12 +20,12 @@ var quizQuestions = [
     option2: "answer2",
     option3: "answer3",
     option4: "answer4",
-    answer: 2,
+    answer: "answer2",
   },
 ];
 var questionEl = document.querySelector("#questions");
 var answerListEl = document.querySelector("#answersList");
-
+var answerMessage = document.querySelector("#answerMessage");
 var answer1 = document.querySelector("#answer1");
 var answer2 = document.querySelector("#answer2");
 var answer3 = document.querySelector("#answer3");
@@ -32,8 +33,11 @@ var answer4 = document.querySelector("#answer4");
 
 var questionCount = 0;
 
+var score
+
 answerListEl.style.display = "none";
 nextButton.style.display = "none"; 
+
 
 //Click button to start quiz
 startButton.addEventListener("click", function (event) {
@@ -41,6 +45,7 @@ startButton.addEventListener("click", function (event) {
   console.log(event);
   showQuiz();
   startButton.style.display = "none";
+  
 });
 
 function showQuiz() {
@@ -48,12 +53,42 @@ function showQuiz() {
   nextButton.style.display = "inline";
   nextButton.addEventListener("click", function (event) {
     event.preventDefault();
-    answerListEl.style.display = "inline";
+    
     questionEl.innerHTML = quizQuestions[questionCount].question;
     answer1.textContent = quizQuestions[questionCount].option1;
     answer2.textContent = quizQuestions[questionCount].option2;
     answer3.textContent = quizQuestions[questionCount].option3;
     answer4.textContent = quizQuestions[questionCount].option4;
+
+    //if quizQuestions.option == quizQuestions.answer {
+      
+    //}
+
     questionCount++;
+   
   });
   }
+  answerListEl.style.display = "inline";
+  function correctAnswers () {
+    
+  }
+
+  
+  
+  //var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+ // var timeLeft = 10 
+
+ var startingTime = 2;
+ var time = startingTime * 60; 
+ var timerEl = document.querySelector(".timer")
+setInterval(displayTimer, 1000);
+
+function displayTimer () {
+  var minutes = Math.floor(time / 60);
+  var seconds = time * 60; 
+  timerEl.textContent = minutes; seconds;
+
+    time--;
+    
+   
+}
